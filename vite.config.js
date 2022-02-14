c920caee85e3c607eee486cda693a11f6e78c43c
eclair <alanbenedictgolpeo@gmail.com>
2022-02-15 02:24:03 +0800

    configuration update

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/mtg/'
+  base: '/mtg/',
   plugins: [vue()],
   resolve: {
     alias: {
