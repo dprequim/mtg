cc7f36690c8ea484ce6f85d9b6574a13754696ec
eclair <alanbenedictgolpeo@gmail.com>
2022-02-18 02:50:35 +0800

    deploy

---
+++ assets/AboutView.8d7bac4c.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./index.5ce7af6b.js";import{o as t,c as o,b as _}from"./vendor.274af062.js";const s={},a={class:"about"},c=_("h1",null,"This is an about page",-1),n=[c];function r(i,u){return t(),o("div",a,n)}var f=e(s,[["render",r]]);export{f as default};
