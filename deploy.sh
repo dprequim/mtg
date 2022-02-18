0738361cc52ec85740fea9688dc30da4b21e831f
eclair <alanbenedictgolpeo@gmail.com>
2022-02-18 10:55:03 +0800

    commented cname

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,7 @@ npm run build
 # navigate into the build output directory
 cd dist
 # if you are deploying to a custom domain
-echo 'metataggames.io' > CNAME
+# echo 'metataggames.io' > CNAME
 
 git init
 git add -A
